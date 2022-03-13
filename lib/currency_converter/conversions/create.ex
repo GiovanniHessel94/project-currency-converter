48443327d97493eafda78e8fcb9bef780c9a2253
Giovanni Hessel <giovanni.hessel@fatec.sp.gov.br>
2022-03-13 18:49:53 -0300

    Implementado módulo responsável pela criação e processamento da conversão juntamente com seus testes

---
+++ lib/currency_converter/conversions/create.ex
@@ -0,0 +1,136 @@
+defmodule CurrencyConverter.Conversions.Create do
+  @moduledoc """
+    Create conversions module.
+
+    Responsible for create the conversion by cleaning the
+    incoming parameters and calling each step of the creation
+    and processing of a conversion.
+  """
+
+  alias CurrencyConverter.{
+    Conversion,
+    Conversions.Convert,
+    Error,
+    ExchangeRates,
+    Repo,
+    Utils
+  }
+
+  alias CurrencyConverter.Decimals.Create, as: CreateDecimal
+  alias Ecto.Changeset
+
+  @operation_defined_keys [
+    "destination_amount",
+    "exchange_rate",
+    "processed_at"
+  ]
+
+  @cant_transform_source_amount_message "source_amount is not a valid number string representation"
+  @source_amount_out_of_precision_range_message "source_amount is not within the precision of 38 digits which 5 are for decimal places"
+
+  @create_decimal_cant_create_decimal_message CreateDecimal.get_cant_create_decimal_message()
+  @create_results_cant_be_trusted_message CreateDecimal.get_results_cant_be_trusted_message()
+
+  def call(params) do
+    with %Changeset{
+           valid?: true
+         } = changeset <- create_inicial_changeset(params),
+         {:ok, %Conversion{} = conversion} <- Conversion.build(changeset),
+         {
+           :ok,
+           %ExchangeRates{} = exchange_rates
+         } <- exchange_rates_client().fetch_exchange_rates(conversion),
+         {:ok, conversion_attrs} <- Convert.call(exchange_rates, conversion),
+         %Changeset{
+           valid?: true
+         } = changeset <- Conversion.changeset(conversion, conversion_attrs),
+         {:ok, %Conversion{} = conversion} <- Repo.insert(changeset) do
+      {:ok, conversion}
+    else
+      %Changeset{valid?: false} = changeset -> {:error, changeset}
+      {:error, _reason} = result -> result
+    end
+  end
+
+  defp create_inicial_changeset(params) do
+    case treat_params(params) do
+      {:ok, new_params} -> Conversion.changeset(new_params)
+      {:error, _reason} = error -> error
+    end
+  end
+
+  defp treat_params(params),
+    do:
+      params
+      |> Utils.remove_keys_from_map(@operation_defined_keys)
+      |> transform_source_amount()
+      |> transform_user_id()
+      |> transform_source_currency()
+      |> transform_destination_currency()
+      |> format_treat_params_return()
+
+  defp transform_source_amount(
+         %{
+           "source_amount" => source_amount
+         } = params
+       ) do
+    case CreateDecimal.call(source_amount) do
+      {:ok, %Decimal{} = source_amount} ->
+        Map.put(params, "source_amount", source_amount)
+
+      {:error, %Error{result: @create_decimal_cant_create_decimal_message}} ->
+        {:error, Error.build(:bad_request, @cant_transform_source_amount_message)}
+
+      {:error, %Error{result: @create_results_cant_be_trusted_message}} ->
+        {:error, Error.build(:bad_request, @source_amount_out_of_precision_range_message)}
+    end
+  end
+
+  defp transform_source_amount(params), do: params
+
+  defp transform_user_id(
+         %{
+           "user_id" => user_id
+         } = params
+       ),
+       do: Map.put(params, "user_id", Utils.value_to_string(user_id))
+
+  defp transform_user_id(params_or_error), do: params_or_error
+
+  defp transform_source_currency(
+         %{
+           "source_currency" => source_currency
+         } = params
+       )
+       when is_binary(source_currency),
+       do: do_transform_currency(params, "source_currency", source_currency)
+
+  defp transform_source_currency(params_or_error), do: params_or_error
+
+  defp transform_destination_currency(
+         %{
+           "destination_currency" => destination_currency
+         } = params
+       )
+       when is_binary(destination_currency),
+       do: do_transform_currency(params, "destination_currency", destination_currency)
+
+  defp transform_destination_currency(params_or_error), do: params_or_error
+
+  defp format_treat_params_return({:error, _reason} = error), do: error
+  defp format_treat_params_return(params), do: {:ok, params}
+
+  defp do_transform_currency(params, currency_key, currency),
+    do:
+      currency
+      |> String.upcase()
+      |> then(&Map.put(params, currency_key, &1))
+
+  defp exchange_rates_client,
+    do:
+      Application.get_env(
+        :currency_converter,
+        :exchange_rates_client,
+        CurrencyConverter.ExchangeRatesApi.Client
+      )
+end
---
+++ test/currency_converter/conversions/create_test.exs
@@ -0,0 +1,194 @@
+defmodule CurrencyConverter.Conversions.CreateTest do
+  use CurrencyConverter.DataCase, async: true
+
+  import CurrencyConverter.Factory
+  import Mox
+
+  alias CurrencyConverter.{
+    Conversion,
+    Conversions.Create,
+    Error,
+    ExchangeRates,
+    ExchangeRatesApi.ClientMock
+  }
+
+  alias Ecto.Changeset
+
+  @fetch_exchange_rates_response {
+    :ok,
+    %ExchangeRates{
+      base: "EUR",
+      exchange_rates: %{
+        "BRL" => 5.516996,
+        "JPY" => 126.131524,
+        "USD" => 1.090251
+      },
+      timestamp: 1_646_780_822
+    }
+  }
+
+  setup_all do
+    base_params =
+      string_params_for(
+        :conversion,
+        source_amount: "10.10",
+        source_currency: "USD",
+        destination_currency: "BRL"
+      )
+
+    {:ok, base_params: base_params}
+  end
+
+  describe "call/1" do
+    setup :verify_on_exit!
+
+    test """
+           when the given params are valid, returns
+           the created conversion
+         """,
+         %{base_params: %{"user_id" => user_id} = base_params} do
+      expect(
+        ClientMock,
+        :fetch_exchange_rates,
+        1,
+        fn %Conversion{
+             source_currency: source_currency,
+             destination_currency: destination_currency
+           } ->
+          assert source_currency == "USD"
+          assert destination_currency == "BRL"
+
+          @fetch_exchange_rates_response
+        end
+      )
+
+      response = Create.call(base_params)
+
+      expected_destination_amount = Decimal.new("51.10903")
+      expected_exchange_rate = Decimal.new("5.06030")
+      expected_source_amount = Decimal.new("10.10000")
+
+      assert {
+               :ok,
+               %Conversion{
+                 created_at: _created_at,
+                 destination_amount: ^expected_destination_amount,
+                 destination_currency: "BRL",
+                 exchange_rate: ^expected_exchange_rate,
+                 id: _id,
+                 processed_at: _processed_at,
+                 source_amount: ^expected_source_amount,
+                 source_currency: "USD",
+                 updated_at: _updated_at,
+                 user_id: ^user_id
+               }
+             } = response
+    end
+
+    test """
+      when the required fields are not present, returns an
+      error with an invalid changeset
+    """ do
+      expect(ClientMock, :fetch_exchange_rates, 0, & &1)
+
+      response = Create.call(%{})
+
+      assert {
+               :error,
+               %Changeset{
+                 errors: [
+                   {
+                     :user_id,
+                     {
+                       "user id must be a positive integer or an UUID",
+                       [validation: :invalid_user_id_format]
+                     }
+                   },
+                   {:user_id, {"can't be blank", [validation: :required]}},
+                   {:source_currency, {"can't be blank", [validation: :required]}},
+                   {:source_amount, {"can't be blank", [validation: :required]}},
+                   {:destination_currency, {"can't be blank", [validation: :required]}}
+                 ],
+                 valid?: false
+               }
+             } = response
+    end
+
+    test """
+           when there is invalid params, returns an
+           error with an invalid changeset
+         """,
+         %{base_params: base_params} do
+      expect(ClientMock, :fetch_exchange_rates, 0, & &1)
+
+      params =
+        base_params
+        |> Map.put("user_id", "hehehe")
+        |> Map.put("source_currency", "CRUZEIRO")
+        |> Map.put("destination_currency", "DOGCOIN")
+
+      response = Create.call(params)
+
+      assert {
+               :error,
+               %Changeset{
+                 errors: [
+                   destination_currency: {
+                     "is invalid",
+                     [validation: :inclusion, enum: _enum_1]
+                   },
+                   source_currency: {
+                     "is invalid",
+                     [validation: :inclusion, enum: _enum_2]
+                   },
+                   user_id: {
+                     "user id must be a positive integer or an UUID",
+                     [validation: :invalid_user_id_format]
+                   }
+                 ],
+                 valid?: false
+               }
+             } = response
+    end
+
+    test """
+           when source_amount is not a valid number
+           representation, returns an error
+         """,
+         %{base_params: base_params} do
+      expect(ClientMock, :fetch_exchange_rates, 0, & &1)
+
+      params = Map.put(base_params, "source_amount", "10.10.1")
+
+      response = Create.call(params)
+
+      assert {
+               :error,
+               %Error{
+                 result: "source_amount is not a valid number string representation",
+                 status: :bad_request
+               }
+             } = response
+    end
+
+    test """
+           when source_amount is out of the precision range, returns an error
+         """,
+         %{base_params: base_params} do
+      expect(ClientMock, :fetch_exchange_rates, 0, & &1)
+
+      params = Map.put(base_params, "source_amount", "1000000000000000000000000000000000.925555")
+
+      response = Create.call(params)
+
+      assert {
+               :error,
+               %Error{
+                 result:
+                   "source_amount is not within the precision of 38 digits which 5 are for decimal places",
+                 status: :bad_request
+               }
+             } = response
+    end
+  end
+end
