f4cf1994e20ecab3d101519e03076a97fed94417
Giovanni Hessel <giovanni.hessel@fatec.sp.gov.br>
2022-03-13 18:54:04 -0300

    Adicionado ao módulo geral da aplicação as funções que serão utilizadas 'globalmente'

--- lib/currency_converter.ex
+++ lib/currency_converter.ex
@@ -6,4 +6,15 @@ defmodule CurrencyConverter do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  alias CurrencyConverter.Conversions.CalculateDestinationAmount, as: CalculateDestinationAmount
+  alias CurrencyConverter.Conversions.Create, as: CreateConversion
+  alias CurrencyConverter.Conversions.Get, as: GetConversion
+
+  defdelegate create_conversion(params), to: CreateConversion, as: :call
+  defdelegate get_conversion_by_user_id(params), to: GetConversion, as: :by_user_id
+
+  defdelegate calculate_destination_amount(exchange_rate, source_amount),
+    to: CalculateDestinationAmount,
+    as: :call
 end
