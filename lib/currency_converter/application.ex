408bffb4ffeb2cdc0a83bf1e80e95c429c3b5c04
Giovanni Hessel <giovanni.hessel@fatec.sp.gov.br>
2022-03-13 18:54:55 -0300

    Adicionado o Task.Supervisor que será responsável pelas execuções do log dde requests e também a inicialização dos módulos de serviço externo

--- lib/currency_converter/application.ex
+++ lib/currency_converter/application.ex
@@ -6,6 +6,7 @@ defmodule CurrencyConverter.Application do
   use Application
 
   alias CurrencyConverter.ElasticSearchApi.ExternalService, as: ElasticSearchApiExternalService
+  alias CurrencyConverter.ExchangeRatesApi.ExternalService, as: ExchangeRatesApiExternalService
 
   @impl true
   def start(_type, _args) do
@@ -17,12 +18,15 @@ defmodule CurrencyConverter.Application do
       # Start the PubSub system
       {Phoenix.PubSub, name: CurrencyConverter.PubSub},
       # Start the Endpoint (http/https)
-      CurrencyConverterWeb.Endpoint
+      CurrencyConverterWeb.Endpoint,
       # Start a worker by calling: CurrencyConverter.Worker.start_link(arg)
       # {CurrencyConverter.Worker, arg}
+      # Start a task supervisor
+      {Task.Supervisor, name: CurrencyConverter.Tasks.Supervisor}
     ]
 
     ElasticSearchApiExternalService.start()
+    ExchangeRatesApiExternalService.start()
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
