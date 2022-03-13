7e0b2699b3544aa239a4108f2e28b184dd0ec463
Giovanni Hessel <giovanni.hessel@fatec.sp.gov.br>
2022-03-13 18:40:56 -0300

    Adicionada algumas funções ao módulo utils e incrementado os testes automatizados

--- lib/currency_converter/utils.ex
+++ lib/currency_converter/utils.ex
@@ -1,4 +1,10 @@
 defmodule CurrencyConverter.Utils do
+  @moduledoc """
+    Utils module.
+
+    Contains functions shared in the app.
+  """
+
   def format_microseconds(microseconds) when microseconds >= 1000,
     do:
       microseconds
@@ -11,4 +17,36 @@ defmodule CurrencyConverter.Utils do
       microseconds
       |> Integer.to_string()
       |> then(fn microseconds -> "#{microseconds}µs" end)
+
+  def remove_keys_from_map(%{} = map, keys), do: Enum.reduce(keys, map, &Map.delete(&2, &1))
+
+  def value_to_string(value) do
+    to_string(value)
+  rescue
+    _e in [Protocol.UndefinedError] -> inspect(value)
+  end
+
+  def get_env(env_name, default \\ nil)
+
+  def get_env(env_name, default) when is_binary(env_name) do
+    env_name_atom =
+      env_name
+      |> String.downcase()
+      |> String.to_atom()
+
+    do_get_env(env_name, env_name_atom, default)
+  end
+
+  def get_env(env_name, default) when is_atom(env_name) do
+    env_name_string =
+      env_name
+      |> Atom.to_string()
+      |> String.upcase()
+
+    do_get_env(env_name_string, env_name, default)
+  end
+
+  defp do_get_env(env_string, env_atom, default) do
+    System.get_env(env_string) || Application.get_env(:currency_converter, env_atom) || default
+  end
 end
--- test/currency_converter/utils_test.exs
+++ test/currency_converter/utils_test.exs
@@ -27,4 +27,167 @@ defmodule CurrencyConverter.UtilsTest do
       assert response == expected_response
     end
   end
+
+  describe "remove_keys_from_map/2" do
+    test """
+      when the keys are present in the map and
+      are in the same format, remove the keys
+    """ do
+      string_map = %{
+        "a" => "a",
+        "b" => "b"
+      }
+
+      keys = ["a"]
+
+      response = Utils.remove_keys_from_map(string_map, keys)
+
+      expected_response = %{"b" => "b"}
+
+      assert response == expected_response
+    end
+
+    test """
+      when the keys are present in the map but aren't
+      in the same format, don't remove the keys
+    """ do
+      atom_map = %{
+        a: "a",
+        b: "b"
+      }
+
+      keys = ["a", "b"]
+
+      response = Utils.remove_keys_from_map(atom_map, keys)
+
+      expected_response = %{
+        a: "a",
+        b: "b"
+      }
+
+      assert response == expected_response
+    end
+
+    test """
+      when the keys aren't present in the
+      map don't remove the any key
+    """ do
+      string_map = %{
+        "a" => "a",
+        "b" => "b"
+      }
+
+      keys = ["c", "d"]
+
+      response = Utils.remove_keys_from_map(string_map, keys)
+
+      assert response == string_map
+    end
+  end
+
+  describe "value_to_string/1" do
+    test """
+      when value can be transformed in string by using the
+      to_string function, returns the string value
+    """ do
+      value = 1000
+
+      response = Utils.value_to_string(value)
+
+      expected_response = to_string(value)
+
+      assert response == expected_response
+    end
+
+    test """
+      when value can't be transformed in string by using the
+      to_string function raising the 'Protocol.UndefinedError'
+      exception, returns the inspected value
+    """ do
+      value = {:banana, true}
+
+      response = Utils.value_to_string(value)
+
+      assert_raise Protocol.UndefinedError, fn ->
+        to_string(value)
+      end
+
+      expected_response = inspect(value)
+
+      assert response == expected_response
+    end
+  end
+
+  describe "get_env/2" do
+    test """
+      when the given env exist in the system, returns its value
+    """ do
+      env = "ELASTIC_SEARCH_API_ENABLED_1"
+
+      System.put_env("ELASTIC_SEARCH_API_ENABLED_1", "true")
+
+      response = Utils.get_env(env)
+
+      expected_response = "true"
+
+      assert response == expected_response
+    end
+
+    test """
+      when the given env exist in the app config, returns its value
+    """ do
+      env = :elastic_search_api_enabled_2
+
+      Application.put_env(:currency_converter, :elastic_search_api_enabled_2, true)
+
+      response = Utils.get_env(env)
+
+      expected_response = true
+
+      assert response == expected_response
+    end
+
+    test """
+      when the given env don't exists but a default
+      value is given, returns the default value
+    """ do
+      env = "ELASTIC_SEARCH_API_ENABLED_3"
+
+      response = Utils.get_env(env, "banana")
+
+      expected_response = "banana"
+
+      assert response == expected_response
+    end
+
+    test """
+      when the given env exist in the system, even if the given
+      key is an atom, returns its value
+    """ do
+      env = :elastic_search_api_enabled_4
+
+      System.put_env("ELASTIC_SEARCH_API_ENABLED_4", "true")
+
+      response = Utils.get_env(env)
+
+      expected_response = "true"
+
+      assert response == expected_response
+    end
+
+    test """
+      when the given env exist in app config, even if the given
+      key is an string, returns its value
+    """ do
+      env = "ELASTIC_SEARCH_API_ENABLED_5"
+
+      Application.put_env(:currency_converter, :elastic_search_api_enabled_5, true)
+
+      response = Utils.get_env(env)
+
+      expected_response = true
+
+      assert response == expected_response
+    end
+  end
 end
